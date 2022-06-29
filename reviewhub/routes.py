f1101b2ed87add26964945adfc1988336a483a3d
BegumDev <ruk90@outlook.com>
2022-06-29 11:36:25 +0000

    Delete current register function and replace with one that stores session in a cookie to enable login and logout functionality

--- reviewhub/routes.py
+++ reviewhub/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from reviewhub import app, db
 from reviewhub.models import User
 
@@ -9,11 +9,16 @@ def home():
     return render_template("home.html")
 
 
-
-# Add a user
+# 1. Add a user
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        # first check if they already registered
+        existing_user = User.query.filter(User.email == request.form.get("email")).all()
+        if existing_user:
+            print("user already exists")
+            return redirect(url_for("register"))
+        # if not, add them
         user = User(
             first_name = request.form.get('first_name'),
             last_name = request.form.get('last_name'),
@@ -22,5 +27,13 @@ def register():
         )
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for("home"))
+        # then put user into session cookie
+        session["user"] = request.form.get('email').lower()
+        return redirect(url_for("my_account", username=session["user"]))
     return render_template('register.html')
+
+
+# 2. My account once registered
+@app.route("/my_account")
+def my_account():
+    return render_template('myaccount.html')
