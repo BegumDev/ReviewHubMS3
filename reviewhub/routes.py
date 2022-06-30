09a5de9c2a480ca121d3790910a3e94c69ad56e7
BegumDev <ruk90@outlook.com>
2022-06-30 12:42:33 +0000

    Add a login page and its function code to enable users to get back into their account.

--- reviewhub/routes.py
+++ reviewhub/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from reviewhub import app, db
 from reviewhub.models import User
 
@@ -23,7 +24,7 @@ def register():
             first_name = request.form.get('first_name'),
             last_name = request.form.get('last_name'),
             email = request.form.get('email'),
-            password = request.form.get('password'),
+            password = generate_password_hash(request.form.get('password')),
         )
         db.session.add(user)
         db.session.commit()
@@ -45,4 +46,29 @@ def my_account(username):
 @app.route("/logout")
 def logout():
     session.pop("user")
-    return redirect(url_for('register'))
+    return redirect(url_for('login'))
+
+
+# Log in to my account
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # check of the user exists
+    if request.method == "POST":
+        existing_user = User.query.filter(User.email == request.form.get("email")).all()
+        if existing_user:
+            print("existing user found")
+            # if they do, check the password
+            if check_password_hash(
+                existing_user[0].password, request.form.get("password")
+                ):
+                print("password found")
+                session["user"] = request.form.get('email')
+                return redirect(url_for("my_account", username=session["user"]))
+            else:
+                print("password not found")
+                return redirect(url_for('login'))
+        else:
+            return redirect(url_for('login'))
+    return(render_template('login.html'))
+
+
