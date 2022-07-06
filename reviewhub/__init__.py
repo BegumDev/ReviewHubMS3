2919d91154b3cc5dd59ecb2455802093b9b1d355
BegumDev <ruk90@outlook.com>
2022-07-06 18:16:50 +0000

    Update Heroku's DATABASE_URL.

--- reviewhub/__init__.py
+++ reviewhub/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+import re
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env
@@ -10,7 +11,9 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
 
 db = SQLAlchemy(app)
 
