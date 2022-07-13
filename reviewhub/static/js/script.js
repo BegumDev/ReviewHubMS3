26d1003b1cc0078d0076be5c2fef51427b1efa42
BegumDev <ruk90@outlook.com>
2022-07-13 22:41:47 +0000

    Disable delete review function for now until a modal can trigger it instead.

--- reviewhub/static/js/script.js
+++ reviewhub/static/js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    $('select').formSelect();
-  });
+// form input
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
+document.addEventListener('DOMContentLoaded', hideModal);
+
+// Global variables
+let modalDisplay = document.querySelector('.modal-container'); // outer modal container
+let confirmDelete = document.querySelector('.confirm-delete'); // delete button
+
+// hide modal on start up
+function hideModal() {
+  modalDisplay.classList.add('hide');
+}
+
+// event listener to trigger modal once delete is clicked
+confirmDelete.addEventListener('click', showModal);
+
+function showModal() {
+  if (modalDisplay.classList.contains('hide')) {
+    modalDisplay.classList.remove('hide');
+  }
+}
