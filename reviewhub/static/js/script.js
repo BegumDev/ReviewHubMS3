db45c7a27c7814fa4fea772e1169e85546b90258
BegumDev <ruk90@outlook.com>
2022-07-14 17:43:54 +0000

    Add jQuery code to show reviews on button click.

--- reviewhub/static/js/script.js
+++ reviewhub/static/js/script.js
@@ -1,10 +1,54 @@
-// form input
+// form input (Materialize)
 $(document).ready(function () {
   $('select').formSelect();
 });
 
-// Modal trigger
+// Modal trigger (Materialize)
 $(document).ready(function(){
   $('.modal').modal();
 });
 
+// ===========================================================================
+
+
+// Hide reviews on main page on page load
+$(document).ready(function(){
+  $('.show-review').hide();
+  $('.close-reviews').hide();
+});
+
+// // Show reviews on button click.
+// $(document).ready(function(){
+//   $('.open-reviews').click(function(){
+//     $('.show-review').show();
+//     $('.open-reviews').hide();
+//     $('.close-reviews').show();
+//   });
+// });
+
+// // Hide reviews on button click.
+// $(document).ready(function(){
+//   $('.close-reviews').click(function(){
+//     $('.show-review').hide();
+//     $('.close-reviews').hide();
+//     $('.open-reviews').show();
+//   })
+// })
+
+// Show reviews on button click.
+$(document).ready(function(){
+  $(".open-reviews").click(function(){
+    $(".show-review").fadeIn(1000);
+    $('.open-reviews').hide();
+    $('.close-reviews').show();
+  });
+});
+
+// Hide reviews on button click
+$(document).ready(function(){
+  $(".close-reviews").click(function(){
+    $(".show-review").fadeOut(1000);
+    $(".open-reviews").show();
+    $('.close-reviews').hide();
+  });
+});
