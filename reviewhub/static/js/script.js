b42c51e575dedf1aeab89c2aed4cd5510378fa4d
BegumDev <ruk90@outlook.com>
2022-07-14 10:49:32 +0000

    Enable deletion of review using a modal.

--- reviewhub/static/js/script.js
+++ reviewhub/static/js/script.js
@@ -3,22 +3,8 @@ $(document).ready(function () {
   $('select').formSelect();
 });
 
-document.addEventListener('DOMContentLoaded', hideModal);
+// Modal trigger
+$(document).ready(function(){
+  $('.modal').modal();
+});
 
-// Global variables
-let modalDisplay = document.querySelector('.modal-container'); // outer modal container
-let confirmDelete = document.querySelector('.confirm-delete'); // delete button
-
-// hide modal on start up
-function hideModal() {
-  modalDisplay.classList.add('hide');
-}
-
-// event listener to trigger modal once delete is clicked
-confirmDelete.addEventListener('click', showModal);
-
-function showModal() {
-  if (modalDisplay.classList.contains('hide')) {
-    modalDisplay.classList.remove('hide');
-  }
-}
